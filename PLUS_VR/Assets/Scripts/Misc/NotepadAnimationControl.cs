b38b3852f13c5db56c41cbce31ffd6b8e41b755e
FairZ <fairsy2104@gmail.com>
2018-07-02 15:01:18 +0100

    Added Notepad to left hand to track objectives

---
+++ PLUS_VR/Assets/Scripts/Misc/NotepadAnimationControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotepadAnimationControl : MonoBehaviour {
+
+    Animator m_animator;
+    public Transform m_camera;
+
+    void Start () {
+        m_animator = gameObject.GetComponent<Animator>();
+	}
+	
+	void Update () {
+        Vector3 toNotepad = transform.position - m_camera.position;
+
+        if(Vector3.Dot(toNotepad.normalized,m_camera.forward) > 0.9f)
+        {
+            m_animator.SetBool("Open", true);
+        }
+        else
+        {
+            m_animator.SetBool("Open", false);
+        }
+	}
+}
--- PLUS_VR/Assets/Scripts/Scenario/ObjectiveSystem.cs
+++ PLUS_VR/Assets/Scripts/Scenario/ObjectiveSystem.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveSystem : MonoBehaviour {
 
-    
+    [HideInInspector]
     public List<Objective> m_objectives;
     public GameObject m_scenarioNPCPrefab;
     public GameObject m_objectiveAreaPrefab;
+
+    public Text m_TitleText;
+    public Text m_descriptionText;
 
     Objective m_currentObjective;
     int m_currentObjectiveIndex = 0;
@@ -17,6 +21,8 @@ public class ObjectiveSystem : MonoBehaviour {
         //load scenario, spawn NPCs??
         m_currentObjective = m_objectives[m_currentObjectiveIndex];
         m_currentObjective.OnBegin();
+        m_TitleText.text = m_objectives[m_currentObjectiveIndex].m_quickTitle;
+        m_descriptionText.text = m_objectives[m_currentObjectiveIndex].m_description;
     }
 
     public void ObjectiveComplete()
@@ -27,9 +33,13 @@ public class ObjectiveSystem : MonoBehaviour {
         {
             m_currentObjective = m_objectives[m_currentObjectiveIndex];
             m_currentObjective.OnBegin();
+            m_TitleText.text = m_currentObjective.m_quickTitle;
+            m_descriptionText.text = m_currentObjective.m_description;
         }
         else
         {
+            m_TitleText.text = "Scenario Complete";
+            m_descriptionText.text = "Look at your score and then take off the Headset";
             //End the scenario
         }
 
